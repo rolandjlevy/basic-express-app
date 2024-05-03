d0688274e4e5acd79875f93c14166f9e921d62ea
Roland Levy <contact@rolandlevy.co.uk>
2024-05-03 22:35:13 +0000

    Added new user endpoint

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const User = require("./User");
 require("dotenv").config();
 
 const { MONGODB_URI, PORT = 3000 } = process.env;
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) =>
-  res.send(`<h1>Home</h1><p><a href='/users'>View users</a></p>`)
-);
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Home</h1>
+    <p><a href='/users'>View users</a></p>
+    <p><a href='/user/add'>Add user</a></p>
+  `);
+});
 
 const formatUsersContent = (data) =>
   data.reduce((acc, item) => {
@@ -61,6 +65,28 @@ app.get("/user/search/:searchTerm", (req, res, next) => {
         return res.status(404).json({ message: "Users not found" });
       }
       res.status(200).json(data);
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+
+app.get("/user/add", (req, res) => {
+  const filePath = path.join(__dirname, "add-user.html");
+  res.sendFile(filePath);
+});
+
+app.post("/user/add", (req, res, next) => {
+  const { username, email, password } = req.body;
+  const newUser = new User({ username, email, password });
+  newUser
+    .save()
+    .then((data) => {
+      return res.status(200).send({
+        data,
+        message: `Thank you ${username}, your user account has been set up. You can now login.`,
+        error: false,
+      });
     })
     .catch((err) => {
       return next(err);
