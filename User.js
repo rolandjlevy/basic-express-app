c2731a8f9d65cb3a13f276c173acbe815519eef8
Roland Levy <contact@rolandlevy.co.uk>
2024-05-04 22:02:07 +0000

    added bcrypt

--- User.js
+++ User.js
@@ -1,24 +1,38 @@
-const mongoose = require('mongoose');
-const moment = require('moment');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const moment = require("moment");
 
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    index: { unique: true }
+    index: { unique: true },
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   date: {
     type: String,
-    default: moment().format('DD/MM/YYYY, hh:mm:ss A')
+    default: moment().format("DD/MM/YYYY, hh:mm:ss A"),
+  },
+});
+
+// use mongoose's middleware to hash password before save
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
   }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
