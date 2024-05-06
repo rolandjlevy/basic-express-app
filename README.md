82b9487e69db2603ce0e99df16c6997ea67aaa5e
Roland Levy <contact@rolandlevy.co.uk>
2024-05-06 15:09:30 +0000

    added register and login endpoints

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Basic MongoDb / Express.js app
+
+### Packages used
+
+- [express-validator](https://express-validator.github.io/docs/guides/getting-started)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const { check, validationResult } = require('express-validator');
 const User = require("./User");
 require("dotenv").config();
 
@@ -90,6 +91,85 @@ app.post("/user/add", (req, res, next) => {
     })
     .catch((err) => {
       return next(err);
+    });
+});
+
+// 422 status code: unable to process the request as it contains invalid data
+const unprocessableEntityStatus = 422;
+
+// Registration
+app.post('/user/register', async (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (password) {
+    await check('confirmedpassword').equals(password).withMessage('passwords do not match').run(req);
+  }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let errorMessage = 'Invalid input: Email must be valid, and the username and password must be 6 - 24 characters long.';
+    if (errors.errors.length == 1 && errors.errors[0].param === 'confirmedpassword') {
+      errorMessage = 'The password and confirmation password do not match.';
+    }
+    const error = new Error(errorMessage);
+    error.status = unprocessableEntityStatus;
+    return next(error);
+  }
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        const error = new Error(`Sorry, the username ${username} already exists. Please try a different one.`);
+        error.status = unprocessableEntityStatus;
+        return next(error);
+      } else {
+        const newUser = new User({ username, email, password });
+        newUser.save()
+        .then(newUserData => {
+          return res.status(200).send({
+            error: false,
+            data: newUserData,
+            message: `Thank you ${username}, your user account has been set up. You can now login.`
+          });
+        })
+      }
+    })
+    .catch(err => {
+      return next(err);
+    });
+});
+
+// Login
+router.post('/user/login', (req, res, next) => {
+  const { username, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Invalid input. The username and password must be completed and be 6 - 24 characters long.');
+    error.status = unprocessableEntityStatus;
+    return next(error);
+  }
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        const error = new Error('Username does not exist');
+        error.status = unprocessableEntityStatus;
+        return next(error);
+      } else {
+        user.comparePassword(password, function(error, isMatch) {
+          if (error) return next(error);
+          if (!isMatch) {
+            const error = new Error('Incorrect password');
+            error.status = unprocessableEntityStatus;
+            return next(error);
+          } else {
+            res.status(200).json({
+              error: false,
+              data: user,
+              message: `Thank you ${username}, you have logged in successfully. Your customer details are shown below.`
+            });
+          }
+        });
+      }
+    })
+    .catch(err => {
+      next(err);
     });
 });
 
